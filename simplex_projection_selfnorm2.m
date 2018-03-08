1618fd290f480559ee23b4ed74166d58d431edfd
Pei-Yi Yang <34266928+yangpy23@users.noreply.github.com>
2018-03-08 10:50:12 +0800

    Update simplex_projection_selfnorm2.m

--- simplex_projection_selfnorm2.m
+++ simplex_projection_selfnorm2.m
@@ -1,5 +1,19 @@
 function w = simplex_projection_selfnorm2(v, b)
+%{
+Please note that TRLR is protected by patent. Any commercial or industrial
+purposes of using TRLR are limited. But it is encouraged for usage of
+study and research.
 
+For any usage of this function, the following papers should be cited as reference, 
+where parameter settings and details are given in the first paper:
+
+[1] Pei-Yi Yang, Zhao-Rong Lai, Xiaotian Wu, Liangda Fang. “Trend Representation 
+Based Log-density Regularization System for Portfolio Optimization”,  
+Pattern Recognition, vol. 76, pp. 14-24, Apr. 2018.
+[2] Zhao-Rong Lai, Dao-Qing Dai, Chuan-Xian Ren, and Ke-Kun Huang. "A Peak Price 
+Tracking-Based Learning System for Portfolio Selection", 
+IEEE Transactions on Neural Networks and Learning Systems, PP(99):1-10, Jun, 2017.
+%}
 
 %v = (v > 0) .* v;
 while(max(abs(v))>1e6)
